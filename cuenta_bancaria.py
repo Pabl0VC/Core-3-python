368d2e4e20bb2d3c53a38825317f9d0192f9bf69
Pabl0VC <pablo_vegacastro@hotmail.com>
2022-06-12 20:20:35 -0400

    Actualizacion Core 3

--- cuenta_bancaria.py
+++ cuenta_bancaria.py
@@ -1,12 +1,13 @@
 class CuentaBancaria:
-    # ¡No olvides agregar algunos valores predeterminados para estos parámetros!
-    def __init__(self, tasa_interés, balance=0): 
+    def __init__(self,  tipo_cuenta, tasa_interés, balance=0,): 
+        self.tipo_cuenta = tipo_cuenta
         self.tasa_interés = tasa_interés
-        self.balance =balance 
+        self.balance = balance
+
+
 
     def depósito(self, monto):
-        self.balance += monto
-
+        self.balance += monto  
         return self
 
 
@@ -18,22 +19,23 @@ class CuentaBancaria:
             self.balance -= monto
         return self
 
+
     def mostrar_info_cuenta(self):
-        print(f" Balance {self.balance}")
+        print(f"Balance ${self.balance}\n")
         return self
 
 
-    def generar_interés(self,interes):
-        # self.balance = self.balance*(1+interes)
-        self.balace= self.balance + self.balance*interes
-        self.mostrar_info_cuenta()
+    def generar_interés(self):
+        print (f"Tipo de cuenta:{self.tipo_cuenta} \nCon una tasa de interés de {self.tasa_interés}:")
+        self.balance= self.balance + (self.tasa_interés)*(self.balance)
+        # self.mostrar_info_cuenta() 
+        # print("\t")
         return self
 
 
-CuentaBancaria1 = CuentaBancaria("balance cuenta 1")
-CuentaBancaria2 = CuentaBancaria("balance")
-CuentaBancaria3 = CuentaBancaria("balans")
+Cuenta_Corriente = CuentaBancaria("Cuenta Corriente", 0.1, 100)
+Cuenta_Ahorro = CuentaBancaria("Cuenta Ahorro",0.2)
 
-CuentaBancaria1.depósito(100).depósito(100).depósito(300).retiro(50).mostrar_info_cuenta().generar_interés(0.1) 
-CuentaBancaria2.depósito(400).depósito(600).retiro(100).retiro(100).retiro(100).retiro(100).mostrar_info_cuenta().generar_interés(0.2)
-CuentaBancaria3.depósito(80).retiro(100).mostrar_info_cuenta()  
+
+Cuenta_Corriente.depósito(100).depósito(100).depósito(300).retiro(50).generar_interés().mostrar_info_cuenta()
+Cuenta_Ahorro.depósito(1000).depósito(600).retiro(100).retiro(100).retiro(100).retiro(100).generar_interés().mostrar_info_cuenta()
